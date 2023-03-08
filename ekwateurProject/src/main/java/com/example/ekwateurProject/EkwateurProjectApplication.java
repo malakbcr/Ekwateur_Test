acc2613a5ba6a3bbb5bcda71f6a71fbc7c3c278e
baccarmalak@gmail.com <baccarmalak@gmail.com>
2023-03-08 23:16:28 +0100

    First commit

---
+++ ekwateurProject/src/main/java/com/example/ekwateurProject/EkwateurProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekwateurProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwateurProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkwateurProjectApplication.class, args);
+	}
+
+}
---
+++ ekwateurProject/src/test/java/com/example/ekwateurProject/EkwateurProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ekwateurProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkwateurProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
